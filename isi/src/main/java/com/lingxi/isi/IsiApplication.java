cbc3dfdcd4189ccaf9bb74aad853511e8a1b211b
gouda <3296257965@qq.com>
2026-03-09 14:11:33 +0800

    Initial commit

--- isi/src/main/java/com/lingxi/isi/IsiApplication.java
+++ isi/src/main/java/com/lingxi/isi/IsiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class IsiApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.run(IsiApplication.class, args);
     }
 
--- scs/src/main/java/com/lingxi/scs/ScsApplication.java
+++ scs/src/main/java/com/lingxi/scs/ScsApplication.java
@@ -1,25 +1,14 @@
 package com.lingxi.scs;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-//@SpringBootApplication
-//public class ScsApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(ScsApplication.class, args);
-//    }
-//
-//}
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
 public class ScsApplication {
 
-    public static void main(String[] args) throws Exception {
+    static void main(String[] args) throws Exception {
         SpringApplication.run(ScsApplication.class, args);
     }
 
